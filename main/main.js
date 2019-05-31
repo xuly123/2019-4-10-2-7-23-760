92b1244f69ab2e43ece501b54aae82ebac0a9ddf
249052342@qq.com <249052342@qq.com>
2019-05-31 22:36:54 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,19 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    var disp=distance(inputs['distance']);//distance price
+	var timp=inputs['parkTime']*0.25;//wait price
+	var price=Math.round(disp+timp);
+	return price;
 };
+
+function distance(dis){
+	var pri=0;
+	if(0<dis&&dis<=2){
+		pri=6;
+	}else if(2<dis&&dis<8){
+		pri=6+(dis-2)*0.8;
+	}else if(dis>=8){
+		pri=10.8+(dis-8)*1.2;
+	}
+	return pri;
+}
